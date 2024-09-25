fd2610fa71b8cfd6f8e23d91f93d1dbda261b715
Bharat Pawar <pawarbr13@gmail.com>
2024-09-25 19:17:31 +0530

    title updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Amplify
+          Welcome to amplify
         </a>
       </header>
     </div>
